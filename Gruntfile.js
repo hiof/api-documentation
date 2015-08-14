cc0ad2e0d9fa913c18a085664cc69180cc6cc166
Kenneth Dahlstrøm <kenneth@keda.no>
2015-08-14 13:36:26 +0200

    Added grunt
    Signed-off-by: Kenneth Dahlstrøm <kenneth@keda.no>

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+    // Loads each task referenced in the packages.json file
+    require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+    require('time-grunt')(grunt);
+
+    // Initiate grunt tasks
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        moment: require('moment'),
+        // Tasks
+        bump: {
+            options: {
+                files: ['package.json'],
+                updateConfigs: [],
+                commit: true,
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: ['-a'],
+                createTag: true,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: false,
+                pushTo: 'upstream',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+                globalReplace: false,
+                prereleaseName: 'beta',
+                regExp: false
+            }
+        }
+
+    });
+
+    grunt.registerTask('default', ['bump']);
+
+
+};
